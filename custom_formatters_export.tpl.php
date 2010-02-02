69fea7603aec5cfc75d521986cf1c70c3630a48c
Stuart Clark <stu@rtclark.net>
2010-02-02 06:47:14 +0000

    #694814 by mauricemengel: Fixed error casued by custom_formatters_token_values(). Added bulk operations. Added pager to overview.

--- custom_formatters_export.tpl.php
+++ custom_formatters_export.tpl.php
@@ -10,9 +10,11 @@
  */
 function <?php print $name ?>_theme() {
   return array(
+<?php foreach($formatters as $formatter) : ?>
     '<?php print $name ?>_formatter_<?php print $formatter->name ?>' => array(
       'arguments' => array('element' => NULL),
     ),
+<?php endforeach; ?>
   );
 }
 
@@ -21,16 +23,20 @@ function <?php print $name ?>_theme() {
  */
 function <?php print $name ?>_field_formatter_info() {
   return array(
+<?php foreach($formatters as $formatter) : ?>
     '<?php print $formatter->name ?>' => array(
       'label' => '<?php print addslashes($formatter->label) ?>',
       'description' => t('<?php print addslashes($formatter->description) ?>'),
       'field types' => array('<?php print implode("', '", unserialize($formatter->field_types)) ?>'),
       'multiple values' => <?php print $formatter->multiple ? 'CONTENT_HANDLE_MODULE' : 'CONTENT_HANDLE_CORE' ?>,
     ),
+<?php endforeach; ?>
   );
 }
+<?php foreach($formatters as $formatter) : ?>
 
 function theme_<?php print $name ?>_formatter_<?php print $formatter->name ?>($element) {
 <?php foreach (split("\n", $formatter->code) as $line) { ?>
   <?php print $line . "\n" ?><?php } ?>
 }
+<?php endforeach; ?>
