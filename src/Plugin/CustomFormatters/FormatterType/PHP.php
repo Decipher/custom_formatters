b35c30bd3943b473a2ae2cdecb69dbb87185d044
Stuart Clark <stu@rtclark.net>
2016-09-01 08:43:12 +1000

    Updated PHP formatter type plugin.

--- src/Plugin/CustomFormatters/FormatterType/PHP.php
+++ src/Plugin/CustomFormatters/FormatterType/PHP.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_formatters\Plugin\CustomFormatters\FormatterType;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\custom_formatters\FormatterTypeBase;
 
 /**
@@ -20,45 +21,31 @@ class PHP extends FormatterTypeBase {
   /**
    * {@inheritdoc}
    */
+  public function settingsForm(array &$form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $form['data']['#description'] = t('Enter the PHP code that will be evaluated. You should NOT include %php tags.<br /><br /><strong>Available arguments:</strong><dl><dt><em><a href=":field_item_list_inerface" target="_blank">FieldItemListInterface</a></em> $items</dt><dd>The field values to be rendered.</dd><dt><em>string</em> $langcode</dt><dd>The language that should be used to render the field.</dd></dt></dl>', [
+      '%php'                      => '<?php ?>',
+      ':field_item_list_inerface' => 'https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FieldItemListInterface.php/interface/FieldItemListInterface',
+    ]);
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-//    global $theme_path, $theme_info, $conf;
-
-//    // Store current theme path.
-//    $old_theme_path = $theme_path;
-
-//    // Restore theme_path to the theme, as long as php_eval() executes,
-//    // so code evaluated will not see the caller module as the current theme.
-//    // If theme info is not initialized get the path from theme_default.
-//    if (!isset($theme_info)) {
-//      $theme_path = drupal_get_path('theme', $conf['theme_default']);
-//    }
-//    else {
-//      $theme_path = dirname($theme_info->filename);
-//    }
-
-//    // Build variables array for formatter.
-//    $variables = array(
-//      '#obj_type' => $obj_type,
-//      '#object'   => $object,
-//      '#field'    => $field,
-//      '#instance' => $instance,
-//      '#langcode' => $langcode,
-//      '#items'    => $items,
-//      '#display'  => $display,
-//    );
-
     ob_start();
     $output = eval($this->entity->get('data'));
     $output = !empty($output) ? $output : ob_get_contents();
     ob_end_clean();
 
     // Preview debugging; Show the available variables data.
-//    if (module_exists('devel') && isset($formatter->preview) && $formatter->preview['options']['dpm']['vars']) {
-//      dpm($variables);
-//    }
-
-//    // Recover original theme path.
-//    $theme_path = $old_theme_path;
+    // @TODO - Re-add when preview functionality re-added.
+    //if (\Drupal::moduleHandler()->moduleExists('devel') && isset($formatter->preview) && $formatter->preview['options']['dpm']['vars']) {
+    //  dpm($variables);
+    //}
 
     return empty($output) ? FALSE : $output;
   }
