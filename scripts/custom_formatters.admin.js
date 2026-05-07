11c524ee3c98b504958f6d330a47d8e3cfcc92a0
Stuart Clark <stu@rtclark.net>
2026-05-07 18:06:39 +1000

    fix: resolve ESLint and Stylelint violations in JS and CSS

--- scripts/custom_formatters.admin.js
+++ scripts/custom_formatters.admin.js
@@ -1,14 +1,20 @@
 /**
  * @file
+ * Custom Formatters admin behaviors.
  */
+
+/* eslint-disable func-names */
+/* global editAreaLoader */
 
 (function ($) {
   Drupal.settings.customFormattersAdmin = {};
 
   Drupal.behaviors.customFormattersAdmin = {
-    attach: function (context) {
+    attach(context) {
       // Set initial fieldset states.
-      if (typeof Drupal.settings.customFormattersAdmin.fieldsets == 'undefined') {
+      if (
+        typeof Drupal.settings.customFormattersAdmin.fieldsets === 'undefined'
+      ) {
         Drupal.settings.customFormattersAdmin.fieldsets = {};
         this.storeStates();
       }
@@ -17,52 +23,68 @@
       }
 
       // EditArea real-time syntax highlighter.
-      if ($('.form-item-code textarea').length >= 1 && typeof editAreaLoader !== 'undefined' && !$('.form-item-code textarea').hasClass('editarea-processed')) {
+      if (
+        $('.form-item-code textarea').length >= 1 &&
+        typeof editAreaLoader !== 'undefined' &&
+        !$('.form-item-code textarea').hasClass('editarea-processed')
+      ) {
         $('.form-item-code textarea').addClass('editarea-processed');
-        syntax = $('.form-item-code textarea').attr('class').match(/syntax-(\w+)\b/m);
+        const syntax = $('.form-item-code textarea')
+          .attr('class')
+          .match(/syntax-(\w+)\b/m);
         editAreaLoader.init({
           id: $('.form-item-code textarea').attr('id'),
           syntax: syntax[1],
           start_highlight: true,
-          allow_resize: "y",
+          allow_resize: 'y',
           allow_toggle: false,
-          toolbar: "*",
+          toolbar: '*',
           word_wrap: false,
-          language: "en",
+          language: 'en',
           replace_tab_by_spaces: 2,
-          change_callback: 'Drupal.behaviors.customFormattersAdmin.customFormattersEAUpdate'
+          change_callback:
+            'Drupal.behaviors.customFormattersAdmin.customFormattersEAUpdate',
         });
 
         // Make sure '#edit-code' gets updated before we preview the formatter.
         $('#engine-wrapper .form-submit').bind('mouseover', function () {
-          Drupal.behaviors.customFormattersAdmin.customFormattersEAUpdate($('.form-item-code textarea').attr('id'))
+          Drupal.behaviors.customFormattersAdmin.customFormattersEAUpdate(
+            $('.form-item-code textarea').attr('id'),
+          );
         });
       }
     },
 
-    detach: function (context) {
+    detach() {
       this.storeStates();
     },
 
-    storeStates: function () {
+    storeStates() {
       $('fieldset').each(function () {
-        id = $(this).attr('id').indexOf('--') > 0 ? $(this).attr('id').substr(0, $(this).attr('id').indexOf('--')) : $(this).attr('id');
-        Drupal.settings.customFormattersAdmin.fieldsets[id] = $(this).hasClass('collapsed');
+        const id =
+          $(this).attr('id').indexOf('--') > 0
+            ? $(this).attr('id').substr(0, $(this).attr('id').indexOf('--'))
+            : $(this).attr('id');
+        Drupal.settings.customFormattersAdmin.fieldsets[id] =
+          $(this).hasClass('collapsed');
       });
     },
 
-    restoreStates: function () {
+    restoreStates() {
       $('fieldset').each(function () {
-        id = $(this).attr('id').indexOf('--') > 0 ? $(this).attr('id').substr(0, $(this).attr('id').indexOf('--')) : $(this).attr('id');
-        if (Drupal.settings.customFormattersAdmin.fieldsets[id] == false) {
+        const id =
+          $(this).attr('id').indexOf('--') > 0
+            ? $(this).attr('id').substr(0, $(this).attr('id').indexOf('--'))
+            : $(this).attr('id');
+        if (Drupal.settings.customFormattersAdmin.fieldsets[id] === false) {
           $(this).removeClass('collapsed');
         }
       });
     },
 
     // Update '#edit-code' with the EditArea code.
-    customFormattersEAUpdate: function (editor_id) {
-      $('#' + editor_id).val(editAreaLoader.getValue(editor_id));
-    }
+    customFormattersEAUpdate(editorId) {
+      $(`#${editorId}`).val(editAreaLoader.getValue(editorId));
+    },
   };
 })(jQuery);
